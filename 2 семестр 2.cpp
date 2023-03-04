a604c2af69d1c384f62bb1927b97cfdcd90d0d13
Герман <116346468+german-skilyagin@users.noreply.github.com>
2023-03-04 14:05:07 +0500

    Add files via upload

--- 2 семестр 2.cpp
+++ 2 семестр 2.cpp
@@ -1,56 +1,156 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <stack>
 using namespace std;
-class Student
-{
-    string a;
-    string a_2;
-    string a_3;
-    int num, resul[5];
-    float midd_s;
-public:
-    Student(string b, string b_2, string b_3, int n, float res[5])
-    {
-        a = b;
-        a_2 = b_2;
-        a_3 = b_3;
-        num = n;
-        float sum = 0;
-        for (int i = 0; i < 5; ++i) {
-            resul[i] = res[i];
-            sum += res[i];
-        }
-        midd_s = sum / 5.0;
-    }
-    bool T() {
-        return (midd_s > 4.2);
-    }
-    void Inf() {
-        cout << a_2 << ' ' << a << ' ' << a_3 << ' ' << num << endl;
-    }
-};
-int main()
-{
-    vector <Student> Gr;
 
-    string c_1, c_2, c_3;
-    int num, flag = 0;
-    float lis[5];
-    for (int i = 0; i < 10; ++i) {
-        cin >> c_1 >> c_2 >> c_3 >> num;
-        for (int j = 0; j < 5; ++j) {
-            cin >> lis[j];
+struct L {
+    char type;
+    double value;
+};
+
+bool Maths(stack <L>& Stack_n, stack <L>& Stack_o, L& item) {
+    double a, b, c;
+    a = Stack_n.top().value;
+    Stack_n.pop();
+    switch (Stack_o.top().type) {
+    case '+':
+        b = Stack_n.top().value;
+        Stack_n.pop();
+        c = a + b;
+        item.type = '0';
+        item.value = c;
+        Stack_n.push(item);
+        Stack_o.pop();
+        break;
+
+    case '-':
+        b = Stack_n.top().value;
+        Stack_n.pop();
+        c = b - a;
+        item.type = '0';
+        item.value = c;
+        Stack_n.push(item);
+        Stack_o.pop();
+        break;
+
+    case '*':
+        b = Stack_n.top().value;
+        Stack_n.pop();
+        c = a * b;
+        item.type = '0';
+        item.value = c;
+        Stack_n.push(item);
+        Stack_o.pop();
+        break;
+
+    case '/':
+        b = Stack_n.top().value;
+        if (a == 0) {
+            cerr << "ERROR!" << endl;
+            return false;
         }
-        Gr.push_back(Student(c_1, c_2, c_3, num, lis));
+        else {
+            Stack_n.pop();
+            c = (b / a);
+            item.type = '0';
+            item.value = c;
+            Stack_n.push(item);
+            Stack_o.pop();
+            break;
+        }
+
+    default:
+        cerr << "ERROR!" << endl;
+        return false;
+        break;
     }
-    for (auto& i : Gr) {
-        if ((i).T()) {
-            flag = 1;
-            (i).Inf();
+    return true;
+}
+
+int getRang(char Ch) {
+    if (Ch == '+' || Ch == '-')return 1;
+    if (Ch == '*' || Ch == '/')return 2;
+    else return 0;
+}
+
+int main() {
+    char Ch;
+    double value;
+    bool flag = 1;
+    stack<L> Stack_n;
+    stack<L> Stack_o;
+    L item;
+    while (1) {
+        Ch = cin.peek();
+        if (Ch == '\n')break;
+        if (Ch == ' ') {
+            cin.ignore();
+            continue;
+        }
+        if (Ch >= '0' && Ch <= '9' || Ch == '-' && flag == 1) {
+            cin >> value;
+            item.type = '0';
+            item.value = value;
+            Stack_n.push(item);
+            flag = 0;
+            continue;
+        }
+        if (Ch == '+' || Ch == '-' && flag == 0 || Ch == '*' || Ch == '/') {
+            if (Stack_o.size() == 0) {
+                item.type = Ch;
+                item.value = 0;
+                Stack_o.push(item);
+                cin.ignore();
+                continue;
+            }
+            if (Stack_o.size() != 0 && getRang(Ch) > getRang(Stack_o.top().type)) {
+                item.type = Ch;
+                item.value = 0;
+                Stack_o.push(item);
+                cin.ignore();
+                continue;
+            }
+            if (Stack_o.size() != 0 && getRang(Ch) <= getRang(Stack_o.top().type)) {
+                if (Maths(Stack_n, Stack_o, item) == false) {
+                    system("pause");
+                    return 0;
+                }
+                continue;
+            }
+        }
+        if (Ch == '(') {
+            item.type = Ch;
+            item.value = 0;
+            Stack_o.push(item);
+            cin.ignore();
+            continue;
+        }
+        if (Ch == ')') {
+            while (Stack_o.top().type != '(') {
+                if (Maths(Stack_n, Stack_o, item) == false) {
+                    system("pause");
+                    return 0;
+                }
+                else continue;
+            }
+            Stack_o.pop();
+            cin.ignore();
+            continue;
+        }
+        else {
+            cout << "ERROR!" << endl;
+            system("pause");
+            return 0;
         }
     }
-    if (flag == 0) {
-        cout << "not student";
+    while (Stack_o.size() != 0) {
+        if (Maths(Stack_n, Stack_o, item) == false) {
+            system("pause");
+            return 0;
+        }
+        else continue;
     }
+
+    cout << "OTVET :   " << Stack_n.top().value << endl;
+    system("pause");
+    return 0;
 }
